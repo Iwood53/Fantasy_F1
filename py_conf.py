307352938bd1b1b3ff8aabaf64ce02f8df5b29f6
Iwood53 <Iwood1350@gmail.com>
2014-07-26 11:00:31 -0400

    added f1 url

--- py_conf.py
+++ py_conf.py
@@ -1,4 +1,5 @@
 driver_csv = 'driver_list.csv'
 pole_pos_csv = 'pole_positions.csv'
 paddy_power_url = 'http://www.paddypower.com/bet/motor-racing/formula-1'
+pole_pos_url = 'http://www.formula1.com/results/season/2014/926/7376/'
 
